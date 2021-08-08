f7599ccf1d7f5c4f6ae3dd4dc3c331a8c5f8721e
lepc1972 <githublepc@gmail.com>
2021-08-08 18:40:20 +0000

    more state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.orm.base import attribute_str
+from models.base_model import BaseModel, base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import sqlalchemy
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
+
+    @property
+    def cities(self):
+        """getter of cities instances"""
+        return self.cities
