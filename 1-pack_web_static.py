85dd9a586a1e8b32732622731a10f97dd1adce4b
lepc1972 <githublepc@gmail.com>
2021-08-25 17:29:17 -0500

    1-pack webstatic

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,3 +3,13 @@
 
 from datetime import datetime
 from fabric.operations import local
+
+
+def do_pack():
+    local("mkdir -p versions")
+    r = local("tar -cvzf versions/web_static_{}.tgz web_static"
+              .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+              capture=True)
+    if r.failed:
+        return None
+    return r
