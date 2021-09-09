479bb73420c0b4d60fe2ad0e72508daa004b8747
lepc1972 <githublepc@gmail.com>
2021-09-09 18:05:48 -0500

    is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB!'
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py_route(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    if type(n) == int:
+        return '%i is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
