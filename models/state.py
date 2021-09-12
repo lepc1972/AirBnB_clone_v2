daaed29232db58e76259df514fb0ff44c303c194
lepc1972 <githublepc@gmail.com>
2021-09-11 19:07:29 -0500

    l

--- models/state.py
+++ models/state.py
@@ -16,13 +16,3 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", back_populates="state")
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            """Getter attribute in case of file storage"""
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
