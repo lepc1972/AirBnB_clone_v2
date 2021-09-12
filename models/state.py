e0d005e984d0524719f29b2c6380281aada826b4
lepc1972 <githublepc@gmail.com>
2021-09-12 13:54:16 -0500

    pep8 state .py

--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship('City', back_populates='state')
     else:
