cd77c768f7f0c3aa421a39a5695bcdada9ec123a
lepc1972 <githublepc@gmail.com>
2021-09-12 16:43:51 -0500

    9

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_one(id=None):
+    states = storage.all(State)
+    if id:
+        key = '{}.{}'.format(State, id)
+        if key in states:
+            states = states[key]
+        else:
+            states = None
+    else:
+        states = storage.all(State).values()
+    return render_template('9-states.html', states=states, id=id)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
