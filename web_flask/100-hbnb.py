dc9400e36eaadf09cf1a6a9865d8b19906fe1d4b
lepc1972 <githublepc@gmail.com>
2021-09-13 17:34:01 -0500

    100 n 10

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -7,15 +7,16 @@ from models.city import City
 from models.amenity import Amenity
 from flask import Flask
 from flask import render_template
+from models.place import Place
 app = Flask(__name__)
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_html():
-    states = storage.all('State').values()
-    cities = storage.all('City').values()
-    amenities = storage.all('Amenity').values()
-    places = storage.all('Place').values()
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    amenities = storage.all(Amenity).values()
+    places = storage.all(Place).values()
     return render_template('100-hbnb.html', **locals())
 
 
