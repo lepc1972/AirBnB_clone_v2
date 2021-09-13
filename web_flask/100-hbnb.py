102cabd037294fa2b58bad2a8051d70e29ef6ecb
lepc1972 <githublepc@gmail.com>
2021-09-12 19:47:27 -0500

    html 100

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starting flask server"""
+
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_html():
+    states = storage.all('State').values()
+    cities = storage.all('City').values()
+    amenities = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    return render_template('100-hbnb.html', **locals())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
